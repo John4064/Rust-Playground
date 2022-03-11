8889e39ed36e1f5ba36869ffab9c4d2434a6aba0
John Parkhurst <jparkhurst120@gmail.com>
2022-03-11 16:26:53 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,21 @@
  * @brief learning about Rust
  */
 
-fn main() {
-    let x = 69;
-    let y = 3;
+//let mut to create a mutable variable
 
-    println!("{}",x+y);
+fn example_type_cast(value: i32){
+    let mut my_base = value;
+    my_base+=5;
+    let my_extra = 3.4;
+    let test = my_base as f64 +my_extra;
+    println!("{}", test);
+}
+
+fn example_array(){
+    let my_arr = [3,4,3,32];
+    println!("{:?}", my_arr);
+}
+
+fn main() {
+    println!("a");
 }
