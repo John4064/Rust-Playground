6a2b6be7fba08c5586b7de098e0c1eb796129bfb
John Parkhurst <jparkhurst120@gmail.com>
2022-08-11 18:07:39 -0400

    Refactor+File Move

--- src/readfiles.rs
+++ src/readfiles.rs
@@ -1,12 +1,13 @@
 
-use std::env;
 use std::fs;
+
+
 pub fn printF(){
     //Prints the file that is read
-
     readF();
 }
 pub fn readF(){
-    let contents = fs::read_to_string("data.yaml")
+    let contents = fs::read_to_string("./data/data.json")
         .expect("Something went wrong reading the file");
+    println!("{}",contents);
 }
